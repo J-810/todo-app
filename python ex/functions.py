7b5c8cb98527a0a0dfb25b921bd19c660d21afbe
jeslet <tjesletthomas000@gmail.com>
2023-10-05 07:18:52 +0200

    ADd print helLo

--- python ex/functions.py
+++ python ex/functions.py
@@ -7,3 +7,4 @@ def get_todos(filepath="../files/todos.txt"):
 def write_todos(todos_arg,filepath="../files/todos.txt"):
     with open(filepath,"w") as file:
         file.writelines(todos_arg)
+        print("hello")
